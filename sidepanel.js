30f20c7f8084b77c7946184b0da7991a1120f72a
hridoy14 <77799804+hridoy14@users.noreply.github.com>
2026-07-21 01:19:28 +0600

    sidepanel.js - enhanced error detection

--- sidepanel.js
+++ sidepanel.js
@@ -599,7 +599,7 @@
         statusBadge +
         "</div>" +
         //SP_SVG.clock + t("sync.waiting") +
-        "<div class=\"sp-sync-status\" id=\"sp-sync\">" + SP_SVG.clock + t("sync.waiting") + "</div>" 
+        "<div class=\"sp-sync-status\" id=\"sp-sync\">" + SP_SVG.clock + t("sync.waiting") + "</div>" +
         "</div>" +
         spTemplateTabs(spActiveTab, spChatHistory.length) +
         "<div id=\"sp-tab-content\"></div>";
